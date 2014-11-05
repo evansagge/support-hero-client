c85b662b0e28027712bebef490ed1c49c8980781
Evan Sagge <evansagge@gmail.com>
2014-11-05 13:05:42 -0500

    Set host in adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
   namespace: 'api/v1',
-  host:      'https://support-hero-api.herokuapp.com/'
+  host:      'https://support-hero-api.herokuapp.com'
 });
